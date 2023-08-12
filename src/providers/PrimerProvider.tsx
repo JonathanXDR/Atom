fddb661d302c421ec333c4d0c90b6f8a364215bf
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-08-13 01:15:56 +0200

    Update PrimerProvider

--- src/providers/PrimerProvider.tsx
+++ src/providers/PrimerProvider.tsx
@@ -1,53 +1,45 @@
 "use client";
-import { BaseStyles, ThemeProvider, theme } from "@primer/react";
-import deepmerge from "deepmerge";
+import { BaseStyles, ThemeProvider } from "@primer/react";
 
-const Atom = deepmerge(theme, {
-  fonts: {
-    normal:
-      "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', sans-serif",
-  },
-});
+// const AtomOneLight = deepmerge(Atom, {
+//   colorSchemes: {
+//     light: {
+//       colors: {
+//         canvas: {
+//           default: "#FAFAFA",
+//         },
+//         fg: {
+//           default: "#383A42",
+//         },
+//         border: {
+//           default: "#DBDBDC",
+//         },
+//       },
+//     },
+//   },
+// });
 
-const AtomOneLight = deepmerge(Atom, {
-  colorSchemes: {
-    light: {
-      colors: {
-        canvas: {
-          default: "#FAFAFA",
-        },
-        fg: {
-          default: "#383A42",
-        },
-        border: {
-          default: "#DBDBDC",
-        },
-      },
-    },
-  },
-});
-
-const AtomOneDark = deepmerge(Atom, {
-  colorSchemes: {
-    dark: {
-      colors: {
-        canvas: {
-          default: "#282C34",
-        },
-        fg: {
-          default: "#ABB2BF",
-        },
-        border: {
-          default: "#181A1F",
-        },
-      },
-    },
-  },
-});
+// const AtomOneDark = deepmerge(Atom, {
+//   colorSchemes: {
+//     dark: {
+//       colors: {
+//         canvas: {
+//           default: "#282C34",
+//         },
+//         fg: {
+//           default: "#ABB2BF",
+//         },
+//         border: {
+//           default: "#181A1F",
+//         },
+//       },
+//     },
+//   },
+// });
 
 const PrimerProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ThemeProvider theme={Atom} preventSSRMismatch>
+    <ThemeProvider colorMode="auto" preventSSRMismatch>
       <BaseStyles>{children}</BaseStyles>
     </ThemeProvider>
   );

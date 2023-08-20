65db52eed44af8d158218181d9c89ee05d8d14c8
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-08-20 18:54:37 +0200

    Cleanup everything

--- src/providers/PrimerProvider.tsx
+++ src/providers/PrimerProvider.tsx
@@ -1,46 +1,47 @@
 'use client';
 import '@/app/tailwind.css';
 import { BaseStyles, ThemeProvider } from '@primer/react';
+import deepmerge from 'deepmerge';
 
-// const AtomOneLight = deepmerge(Atom, {
-//   colorSchemes: {
-//     light: {
-//       colors: {
-//         canvas: {
-//           default: "#FAFAFA",
-//         },
-//         fg: {
-//           default: "#383A42",
-//         },
-//         border: {
-//           default: "#DBDBDC",
-//         },
-//       },
-//     },
-//   },
-// });
+const AtomOneLight = deepmerge(Atom, {
+  colorSchemes: {
+    light: {
+      colors: {
+        canvas: {
+          default: '#FAFAFA',
+        },
+        fg: {
+          default: '#383A42',
+        },
+        border: {
+          default: '#DBDBDC',
+        },
+      },
+    },
+  },
+});
 
-// const AtomOneDark = deepmerge(Atom, {
-//   colorSchemes: {
-//     dark: {
-//       colors: {
-//         canvas: {
-//           default: "#282C34",
-//         },
-//         fg: {
-//           default: "#ABB2BF",
-//         },
-//         border: {
-//           default: "#181A1F",
-//         },
-//       },
-//     },
-//   },
-// });
+const AtomOneDark = deepmerge(Atom, {
+  colorSchemes: {
+    dark: {
+      colors: {
+        canvas: {
+          default: '#282C34',
+        },
+        fg: {
+          default: '#ABB2BF',
+        },
+        border: {
+          default: '#181A1F',
+        },
+      },
+    },
+  },
+});
 
 const PrimerProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ThemeProvider colorMode="auto" preventSSRMismatch>
+    <ThemeProvider colorMode="light" preventSSRMismatch>
       <BaseStyles className="w-full h-full">{children}</BaseStyles>
     </ThemeProvider>
   );
